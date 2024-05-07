803c80217d037ff7605605e91288b1a6b3acb016
Jose David <lujan9495@gmail.com>
2024-05-06 20:45:16 -0500

    Validando el presupuesto

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import BudgetForm from "./components/BudgetForm"
 function App() {
 
   return (
-    <>
+    <div className=" bg-slate-100 min-h-screen">
       <header className=" bg-blue-600 py-8 max-h-72">
         <h1 className=" uppercase text-center font-black text-4xl text-white">
           Planificador de Gastos
@@ -13,7 +13,7 @@ function App() {
       <div className=" max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
         <BudgetForm />
       </div>
-    </>
+    </div>
   )
 }
 
--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent } from "react"
+import { useState, ChangeEvent, useMemo } from "react"
 
 export default function BudgetForm() {
   
@@ -6,8 +6,11 @@ export default function BudgetForm() {
 
     const handleChange = (e : ChangeEvent<HTMLInputElement>) => {
         setBudget(+e.target.value)
-
     }
+
+    const isValid = useMemo(() => {
+        return (isNaN(budget)) || budget <= 0
+    }, [budget])
 
     return (
         <form className=" space-y-5">
@@ -29,7 +32,8 @@ export default function BudgetForm() {
             <input 
                 type="submit" 
                 value="Guardar Presupuesto"
-                className=" bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase"
+                className=" bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40"
+                disabled={isValid}
             />
         </form>
   )
