e513465f86fc4029f664cdcf963f6728d2794ec9
Jose David <lujan9495@gmail.com>
2024-10-30 20:49:01 -0500

    Mostrando los gastos, función para formatear fecha, componente para mostrar gastos

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,16 @@
-export const formatCurrency = (cantidad : number) => {
-  return cantidad.toLocaleString('es-CO', {
-      style: 'currency',
-      currency: 'COP'
-  })
+export function formatCurrency(cantidad : number) {
+  return new Intl.NumberFormat('es-CO', {style: 'currency', currency: 'COP'}).format(cantidad)
+}
+
+export function formatDate(dateStr: string) : string {
+
+  const dateObj = new Date(dateStr)
+  const options : Intl.DateTimeFormatOptions = {
+    weekday: 'long',
+    year: 'numeric', 
+    month: 'long',
+    day: 'numeric'
+  }
+
+  return new Intl.DateTimeFormat('es-CO', options).format(dateObj)
 }
