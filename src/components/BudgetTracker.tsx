5a49eb9c6c9505c711ab51755602c8bbfb4f72d6
Jose David <lujan9495@gmail.com>
2024-11-06 09:37:07 -0500

    Agregando grafica circular interactiva y sus propiedas y estilos

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,6 +1,8 @@
+import { CircularProgressbar, buildStyles} from "react-circular-progressbar" 
 import { useMemo } from "react";
 import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function BudgetTracker() {
 
@@ -9,10 +11,22 @@ export default function BudgetTracker() {
   const totalExpenses = useMemo(() => state.expenses.reduce((total, expenses) => expenses.amount + total, 0), [state.expenses])
   const remainingBudget = state.budget - totalExpenses
 
+  //Porcentaje de la barra
+  const percentage = +((totalExpenses / state.budget) * 100).toFixed(2)
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className=" flex justify-center">
-        <img src="/grafico.jpg" alt="Gráfica de gastos" />
+        <CircularProgressbar 
+          value={percentage}
+          styles={buildStyles({
+            pathColor: percentage >= 100 ? '#DC2626' : '#3b82f6',
+            trailColor: '#F5F5F5',
+            textSize: 8,
+            textColor: percentage >= 100 ? '#DC2626' : '#3b82f6',
+          })}
+          text={`${percentage}%  Gastado`}
+          />
       </div>
 
       <div className="flex flex-col justify-center items-center gap-8">
