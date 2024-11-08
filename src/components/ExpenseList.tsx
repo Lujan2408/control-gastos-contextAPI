579a146f8033b3099e25e4e3b94e8cc4a78e92df
Jose David <lujan9495@gmail.com>
2024-11-08 12:33:45 -0500

    Mostrar los gastos de la categoría seleccionada

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -8,12 +8,14 @@ export default function ExpenseList() {
 
   const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses])
 
+  const filteredCategory = state.currentCategory ? state.expenses.filter( expense => expense.category === state.currentCategory ) : state.expenses
+
   return (
     <div className="mt-10 bg-white shadow-lg rounded-lg p-10"> 
       { isEmpty ? <p className=" text-gray-600 font-bold text-2xl">No hay gastos Registrados</p> : (
         <>
           <p className=" text-gray-600 font-bold text-2xl my-5">Listado de Gastos</p>
-          {state.expenses.map( expense => (
+          {filteredCategory.map( expense => (
             <ExpenseDetails
               key={expense.id} 
               expense={expense}
